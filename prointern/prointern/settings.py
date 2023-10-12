78c71efb5df48dfbb212654b6b4850eee852a800
parvathyg17 <parvathyg05@gmail.com>
2023-10-12 15:47:51 +0530

    asd

--- prointern/prointern/settings.py
+++ prointern/prointern/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'prointern.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
